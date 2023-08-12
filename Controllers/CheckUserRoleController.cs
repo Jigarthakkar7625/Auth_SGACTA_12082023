0b90ee4e687bfc3adb210c6d24484bcabbae41bf
Jigar1992 <jigar.thakkar@tatvasoft.com>
2023-08-12 19:43:54 +0530

    Add project files.

---
+++ Controllers/CheckUserRoleController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auth_SGACTA.Controllers
+{
+    public class CheckUserRoleController : Controller
+    {
+        // GET: CheckUserRole
+
+        [Authorize(Roles = "Admin,User")]
+        public ActionResult Index()
+        {
+
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                        .FirstOrDefault(c => c.Type == "Gender").Value;
+            //var LogTime = identity.Claims
+            //            .FirstOrDefault(c => c.Type == "LoggedOn").Value;
+
+            return View();
+        }
+
+        // GET: CheckUserRole/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "User")]
+        // GET: CheckUserRole/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckUserRole/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckUserRole/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckUserRole/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckUserRole/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckUserRole/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿using Auth_SGACTA.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auth_SGACTA.Startup))]
+namespace Auth_SGACTA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        private void CreateRoles()
+        {
+
+            var context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            //var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //role.Name = "User";
+            //roleManager.Create(role);
+
+            var role1 = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            role1.Name = "Admin";
+            roleManager.Create(role1);
+        }
+
+    }
+}
